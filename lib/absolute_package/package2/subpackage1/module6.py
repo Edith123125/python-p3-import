6dbbe4adc8f645a8395f0bbb2ab14d247cb2c4c7
Edith Gatwiri <edithgatwiri70@gmail.com>
2025-02-20 14:13:15 +0300

    imports

--- lib/absolute_package/package2/subpackage1/module6.py
+++ lib/absolute_package/package2/subpackage1/module6.py
@@ -1,2 +1,9 @@
-def function1():
-    print('Function 1 in module 6')
+
+
+    from ..module6 import function1 
+
+def call_function():
+    function1()
+
+if __name__ == "__main__":
+    call_function()
